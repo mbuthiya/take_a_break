626f03273b997bd56272d4963b8935532d4ab35e
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-05-26 15:53:36 +0300

    Imported the time module that allows us to delay the program after some time

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #webbrowser is a module that allows python to open a web browser
 import webbrowser
+# time module allows us to access different functions
+import time
 
 webbrowser.open('https://www.youtube.com/watch?v=OpIQNxiKJoE')
