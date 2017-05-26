a3d5c622ff5130400d05333bfd8dbb88fcfab599
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-05-26 16:03:00 +0300

    put the youtube url in a variable

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import webbrowser
 # time module allows us to access different functions
 import time
 
-webbrowser.open('https://www.youtube.com/watch?v=OpIQNxiKJoE')
+youtube_video = "https://www.youtube.com/watch?v=1Wh8RzcQZr4"
+time.sleep(3)
+webbrowser.open(youtube_video)
