d386a43ee62cfd976f970c99b26b62c8995166ee
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-05-26 22:10:14 +0300

    Added an option to insert a resting period and also to add their name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#webbrowser is a module that allows python to open a web browser
+
 import webbrowser
 # time module allows us to access different functions
 import time
@@ -6,16 +6,43 @@ import time
 #random 
 import random
 
-youtube_videos = ["https://www.youtube.com/watch?v=1Wh8RzcQZr4","https://www.youtube.com/watch?v=ssRIHxNV8Ls","https://www.youtube.com/watch?v=geqVuYmo8Y0"]
+links_list = []
+#Input to get name
+name = input("What is your Name? ")
 
-
+#input for the desired breaks you want
 breaks_counter = input('How many breaks do you want to take today? ')
 breaks_counter_int = int(breaks_counter)
 
+#timer how long do you want to work in intervals
+timer_work = input("How many minutes do you want to work? ")
+time_in_seconds = int(timer_work) * 60
+
+timer_break = input("How many minutes do you want to rest? ")
+time_break_in_seconds = int(timer_break) * 60
+
+
+links_list_lenght=len(links_list)
+mini_counter = breaks_counter_int
+
+#looping through to add links you might find useful
+while(links_list_lenght < breaks_counter_int):
+    
+    web_link = input(f"Please insert {mini_counter} link to anything you find interesting ")
+    links_list.append(web_link)
+    links_list_lenght += 1
+    mini_counter -= 1
+
+
+#looping to get time work intervals and also to pick a random link
 counter = 0;
 while(counter < breaks_counter_int):
-    time.sleep(2)
-    random.shuffle(youtube_videos)
-    webbrowser.open(youtube_videos[0])
+    print(f"Its work time {name}")
+    time.sleep(time_in_seconds)
+    random.shuffle(links_list)
+    webbrowser.open(links_list[-1])
+    links_list.pop()
+    print(f"Its rest time {name}")
+    time.sleep(time_break_in_seconds)
     counter+=1
 
